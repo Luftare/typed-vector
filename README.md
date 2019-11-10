62bcdcd747b5ae478f3e1f25e53743b34231ae9f
Ilmari Koskinen <ilmari.koskinen1@gmail.com>
2019-11-10 14:12:16 +0200

    Add constructor

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # typed-vector
+
+It's a 2d vector library written in typescript.
+
+## API
+
+### Vector(x: number, y: number)
+`````ts
+const vector = new Vector(1, 2);
+`````
--- __tests__/Vector.test.ts
+++ __tests__/Vector.test.ts
@@ -4,4 +4,10 @@ describe('Vector', () => {
   it('Should exist', () => {
     expect(Vector).toBeDefined();
   });
+
+  it('Can be initialized with scalars', () => {
+    const vector = new Vector(5, 4);
+
+    expect(vector).toEqual({ x: 5, y: 4 });
+  })
 });
